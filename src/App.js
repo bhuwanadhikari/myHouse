cb29c161b79833fb51e45165a1a4740da305a5ed
bhuwanadhikari <adhikaribhuwan97@gmail.com>
2018-08-22 19:03:21 +0545

    Mobile view still unfixed

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import UnderConstruction from './components/UnderConstruction/UnderConstruction'
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <UnderConstruction/>
+            </div>
+        );
+    }
+}
+
+export default App;
